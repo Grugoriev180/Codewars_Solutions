1871cae5c44a6131d580b5b5ff851a4346fba0d1
KOSTIANTYN HRYHORIEV <100276903+Grugoriev180@users.noreply.github.com>
2023-04-26 13:38:39 +0300

    Add files via upload

---
+++ 8kyu/StringTemplates-BugFixing#5.cs
@@ -0,0 +1,14 @@
+//Oh no! Timmy hasn't followed instructions very carefully and forgot how to use the new String 
+//Template feature, Help Timmy with his string template so it works as he expects!
+
+//my solution
+
+using System;
+
+public static class Kata 
+{
+    public static string buildString(string[] args)
+    {
+      return String.Format("I like {0}!", String.Join(", ", args));
+    }
+}
