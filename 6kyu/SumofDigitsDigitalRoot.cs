cd5be834c3752981a860115a860450413dc0b749
KOSTIANTYN HRYHORIEV <100276903+Grugoriev180@users.noreply.github.com>
2023-05-29 12:43:50 +0300

    Add files via upload

---
+++ 6kyu/SumofDigitsDigitalRoot.cs
@@ -0,0 +1,17 @@
+// DESCRIPTION:
+// Digital root is the recursive sum of all the digits in a number.
+
+// Given n, take the sum of the digits of n. If that value has more than one digit, continue reducing in this way until 
+// a single-digit number is produced. The input will be a non-negative integer.
+
+
+
+//my solution
+
+public class Number
+{
+  public static int DigitalRoot(long n)
+  {
+    return (int)(1+(n-1)%9);
+  }
+}
